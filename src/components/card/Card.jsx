eaa7584e7438fa4609ff19f94700148fcf963a70
Andres Torres <atorres981@gmail.com>
2024-08-21 18:50:54 +0500

    Static file bug fixes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -15,8 +15,8 @@ const Card = ({ product }) => {
   const active = state[product.name];
 
   const cardStyle = {
-    // backgroundImage: `url(${product.image[src]})`,
-    backgroundImage: `url("/assets/images/favicon-32x32.png")`,
+    backgroundImage: `url(${product.image[src]})`,
+    // backgroundImage: `url("/assets/images/favicon-32x32.png")`,
     outline: active ? `2px solid ${colors["red"]}` : "none",
   };
 
