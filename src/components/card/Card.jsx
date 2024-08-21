33bc0da563d12b189491922ece9911bfecf2453d
Andres Torres <atorres981@gmail.com>
2024-08-21 17:57:04 +0500

    Updating

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -14,9 +14,10 @@ const Card = ({ product }) => {
 
   const active = state[product.name];
 
+  const imageUrl = encodeURI(product.image[src]);
   const cardStyle = {
     // backgroundImage: `url(${product.image[src]})`,
-    backgroundImage: `url("../../assets/images/image-baklava-mobile.jpg")`,
+    backgroundImage: `url(${imageUrl})`,
     outline: active ? `2px solid ${colors["red"]}` : "none",
   };
 
